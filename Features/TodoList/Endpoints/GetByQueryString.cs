19e68184827242d1e93a1d5abf38b11990cf053c
chunglijung <andy28572463@gmail.com>
2024-04-27 16:40:20 +0800

    feat: 新增 dymic queryString

--- Features/TodoList/Endpoints/GetByQueryString.cs
+++ Features/TodoList/Endpoints/GetByQueryString.cs
@@ -3,10 +3,17 @@
 public class GetByQueryString : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder endpoint) => endpoint.MapGet("/GetByQueryString", HandleAsync);
-    public record QueryTodoListRequest(string? Name);
+    public record QueryTodoListRequest(Guid? TodoId, string? Name);
 
     private static async Task<Ok<ResultResponse<List<Domain.Entities.TodoList>>>> HandleAsync(
         [AsParameters] QueryTodoListRequest request,
-        TodoContext todoContext) =>
-        TypedResults.Ok(ResponseFactory.CreateSuccessResponse(await todoContext.TodoList.ToListAsync()));
+        TodoContext todoContext)
+    {
+        var isParseTodoId = Guid.TryParse(request.TodoId.ToString(), out var todoId);
+        var query = todoContext.TodoList
+            .AsQueryable()
+            .Where(x => String.IsNullOrEmpty(request.Name) || x.Name == request.Name)
+            .Where(x => !isParseTodoId || x.TodoId == todoId);
+        return TypedResults.Ok(ResponseFactory.CreateSuccessResponse(await query.ToListAsync()));
+    }
 }
